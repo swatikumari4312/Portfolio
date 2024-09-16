38b19d0703c94720609a32f64721d96c25a63f5a
Swati kumari <153591610+swatikumari4312@users.noreply.github.com>
2024-09-17 01:00:16 +0530

    Delete public directory

--- public/new.js
+++
@@ -1,9 +0,0 @@
-let typed= new Typed(".text",{
-    strings:[" Full Stack developer " ,"Website Creation","Expertise in DSA,"],
-    typeSpeed: 10,
-    backSpeed: 50,
-    backDelay: 100,
-    loop: true
-});
-
-
