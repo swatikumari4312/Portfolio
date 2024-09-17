bf6d469ed8873fc6d8c50be72f34c532f1e1efa2
Swati kumari <153591610+swatikumari4312@users.noreply.github.com>
2024-09-17 11:05:10 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,9 @@
+let typed= new Typed(".text",{
+    strings:[" Full Stack developer " ,"Website Creation","Expertise in DSA,"],
+    typeSpeed: 10,
+    backSpeed: 50,
+    backDelay: 100,
+    loop: true
+});
+
+
